4e561e43f54a6992a794b5f2809720f3a735b967
owlda <e1895226@cmaisonneuve.qc.ca>
2019-05-15 10:11:30 -0400

    initial commit to QSort class

---
+++ quicksort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quicksort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QSort qSort = new QSort();
+            int[] array = new[] {123, 1, 3, 4, 12};
+            qSort.QuickSort(array, 0, array.Length-1);
+            foreach (var i in array)
+            {
+                Console.Write(i.ToString()+" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ quicksort/QSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quicksort
+{
+    class QSort
+    {
+        
+        public void QuickSort(int[] array, int min, int max)
+        {
+            int pivot = -1;
+            if (min < max)
+            {
+                pivot = GetPartition(array, min, max);
+                if (pivot > 1)
+                {
+                    QuickSort(array, min, pivot -1);
+                }
+
+                if (pivot + 1 < max)
+                {
+                    QuickSort(array, pivot+1, max);
+                }
+            }
+        }
+
+        private void Swap(int[] array, int index1, int index2)
+        {
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+        
+
+        private int GetPartition(int[] array, int min, int max)
+        {
+            int pivot = -1;
+            pivot = array[min];
+            while (true)
+            {
+                while (array[min] < pivot)
+                {
+                    min++;
+                }
+
+                while (array[max] > pivot)
+                {
+                    max--;
+                }
+
+                if (min < max)
+                {
+                    Swap(array, min, max);
+                }
+                else
+                {
+                    return max;
+                }
+            }
+        }
+
+    }
+}
