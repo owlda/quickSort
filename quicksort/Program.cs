ef987b5fc775b3474b5bb7c54be99603ac8526cd
owlda <e1895226@cmaisonneuve.qc.ca>
2019-05-15 17:51:49 -0400

    added Arrays.Sort()

--- quicksort/Program.cs
+++ quicksort/Program.cs
@@ -12,10 +12,20 @@ namespace quicksort
         {
             QSort qSort = new QSort();
             int[] array = new[] {123, 1, 3, 4, 12};
+            int[] arrayNew = new[] { 123, 1, 3, 4, 12, 1,334};
             qSort.QuickSort(array, 0, array.Length-1);
             foreach (var i in array)
             {
                 Console.Write(i.ToString()+" ");
+            }
+
+            Console.ReadKey();
+
+            Array.Sort(arrayNew); 
+
+            foreach (var i in arrayNew)
+            {
+                Console.Write(i.ToString() + " ");
             }
 
             Console.ReadKey();
